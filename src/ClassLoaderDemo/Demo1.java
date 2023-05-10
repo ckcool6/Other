e3f56402a63f7d3fb5df48d08acdc77cd95b7d16
rzdtjb <rzdtjb@gmail.com>
2023-05-10 20:57:42 +0800

    Reflect

---
+++ src/ClassLoaderDemo/Demo1.java
@@ -0,0 +1,13 @@
+package ClassLoaderDemo;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+        ClassLoader classLoader = systemClassLoader.getParent();
+        ClassLoader classLoader1 = classLoader.getParent();
+
+        System.out.println(systemClassLoader);
+        System.out.println(classLoader);
+        System.out.println(classLoader1);
+    }
+}
---
+++ src/ClassLoaderDemo/ReflectDemo1.java
@@ -0,0 +1,19 @@
+package ClassLoaderDemo;
+
+public class ReflectDemo1 {
+    public static void main(String[] args) throws ClassNotFoundException {
+        //1
+        Class<?> aClass = Class.forName("ClassLoaderDemo.Demo1");
+        System.out.println(aClass);
+
+        //2
+        Class<Demo1> demo1Class = Demo1.class;
+        System.out.println(demo1Class);
+
+        //3
+        Demo1 demo1 = new Demo1();
+        Class<? extends Demo1> aClass1 = demo1.getClass();
+        System.out.println(aClass1);
+
+    }
+}
