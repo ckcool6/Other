b4a6c9ab1028d37fd6ce6965a3a523e0b37bc850
rzdtjb <rzdtjb@gmail.com>
2023-05-12 13:32:41 +0800

    Log4j

--- src/UnitTestAndLog/LogDemo1.java
+++ src/UnitTestAndLog/LogDemo1.java
@@ -1,5 +1,19 @@
 package UnitTestAndLog;
 
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class LogDemo1 {
+    //private static final Logger LOGGER = Logger.getLogger(LogDemo1.class);
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogDemo1.class);
+    public static void main(String[] args) {
+        LOGGER.debug("debug info");
+        LOGGER.info("info");
+        LOGGER.warn("warn info");
+        LOGGER.error("error info");
+
+    }
 
 }
